74213213a1c5e24b7d665324fc8100481090ad0b
Christopher-Kwiatkowski <shannonhurley@Shannons-MacBook-Air.local>
2020-04-27 23:27:38 -0600

    backend routes working, error handleing well, postman tests the routes correctly

--- starter/app.py
+++ starter/app.py
@@ -2,13 +2,205 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import json
+from models import db_drop_and_create_all, setup_db, Movie, Actor
+from auth import AuthError, requires_auth
+
+# except Exception as error:
+#     print("Oops! An exception has occured:", error)
+#     print("Exception TYPE:", type(error))
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    db_drop_and_create_all()
+
+    @app.after_request
+    def after_request(response):
+        # response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-All-Methods',
+                             'GET, POST, PATCH, DELETE, OPTIONS')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    @app.route('/actors', methods=['GET'])
+    @requires_auth("get:actors")
+    def get_all_actors(jwt):
+        try:
+            print('actors')
+            actors = Actor.query.all()
+            all_actors = [actor.format() for actor in actors]
+            print(all_actors)
+            return jsonify({
+                "success": True,
+                "actors": all_actors
+            })
+        except:
+            abort(401)
+
+    @app.route('/movies', methods=['GET'])
+    @requires_auth("get:movies")
+    def get_all_movies(jwt):
+        try:
+            print('movies')
+            movies = Movie.query.all()
+            all_movies = [movie.format() for movie in movies]
+            print(all_movies)
+            return jsonify({
+                "success": True,
+                "movies": all_movies
+            })
+        except:
+            abort(401)
+
+    @app.route('/actors', methods=["POST"])
+    @requires_auth("post:actors")
+    def add_new_actor(jwt):
+        try:
+            new_actor = json.loads(request.data)
+            name = new_actor['name']
+            age = new_actor['age']
+            gender = new_actor['gender']
+            actor = Actor(name=name, age=age, gender=gender)
+            array_of_actor = []
+            array_of_actor.append(
+                {'name': name, 'age': age, 'gender': gender})
+            actor.insert()
+            return jsonify({
+                'success': True,
+                'actors': array_of_actor
+            })
+        except:
+            abort(401)
+
+    @app.route('/movies', methods=["POST"])
+    @requires_auth("post:movies")
+    def add_new_movie(jwt):
+        try:
+            new_movie = json.loads(request.data)
+            title = new_movie['title']
+            release = new_movie['release']
+            movie = Movie(title=title, release=release)
+            array_of_movie = []
+            array_of_movie.append({'title': title, 'release': release})
+            movie.insert()
+            return jsonify({
+                'success': True,
+                'actors': array_of_movie
+            })
+        except:
+            abort(401)
+
+    @app.route('/actors/<int:actor_id>', methods=["PATCH"])
+    @requires_auth("patch:actors")
+    def update_actor(jwt, actor_id):
+        try:
+            actor = Actor.query.get(actor_id)
+            if not actor:
+                abort(404)
+            update_to_actor = json.loads(request.data)
+            if hasattr('update_to_actor', 'name'):
+                actor.name = update_to_actor['name']
+            if hasattr('update_to_actor', 'age'):
+                actor.age = update_to_actor['age']
+            if hasattr('update_to_actor', 'gender'):
+                actor.gender = update_to_actor['gender']
+            updated_actor = [
+                {'id': actor.id, 'name': actor.name, 'age': actor.age, 'gender': actor.gender}]
+            actor.update()
+            return jsonify({
+                'success': True,
+                'actors': updated_actor
+            })
+        except:
+            abort(401)
+
+    @app.route('/movies/<int:movie_id>', methods=["PATCH"])
+    @requires_auth("patch:movies")
+    def update_movie(jwt, movie_id):
+        try:
+            movie = Movie.query.get(movie_id)
+            if not movie:
+                abort(404)
+            update_to_movie = json.loads(request.data)
+            if hasattr('update_to_movie', 'title'):
+                movie.title = update_to_movie['title']
+            if hasattr('update_to_movie', 'release'):
+                movie.release = update_to_movie['release']
+            updated_movie = [
+                {'id': movie.id, 'title': movie.title, 'release': movie.release}]
+            movie.update()
+            return jsonify({
+                'success': True,
+                'movies': updated_movie
+            })
+        except:
+            abort(401)
+
+    @app.route('/actors/<int:actor_id>', methods=["Delete"])
+    @requires_auth("delete:actors")
+    def delete_actor(jwt, actor_id):
+        try:
+            actor = Actor.query.get(actor_id)
+            if not actor:
+                abort(404)
+            actor_id = actor.id
+            actor.delete
+            return jsonify({
+                'success': True,
+                'actors': actor_id
+            })
+        except:
+            abort(401)
+
+    @app.route('/movies/<int:movie_id>', methods=["Delete"])
+    @requires_auth("delete:movies")
+    def delete_movie(jwt, movie_id):
+        try:
+            movie = Movie.query.get(movie_id)
+            if not movie:
+                abort(404)
+            movie_id = movie.id
+            movie.delete
+            return jsonify({
+                'success': True,
+                'movies': movie_id
+            })
+        except:
+            abort(401)
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Not Found"
+        }), 404
+
+    @app.errorhandler(AuthError)
+    def auth_error(error):
+        error_code = error.status_code
+        description = error.error
+        return jsonify({
+            "success": False,
+            "error": error_code,
+            "message": description
+        }), 401
+
+    return app
+
 
 APP = create_app()
 
--- starter/models.py
+++ starter/models.py
@@ -1,1 +1,92 @@
+import os
+from sqlalchemy import Column, String, create_engine, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+database_path = os.environ.get('DATABASE_URL')
+if not database_path:
+    database_name = "agency"
+    database_path = "postgres://{}/{}".format(
+        'shannonhurley@localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    release = Column(String(80), nullable=False)
+
+    def __init__(self, title, release=""):
+        self.title = title
+        self.release = release
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release': self.release}
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
